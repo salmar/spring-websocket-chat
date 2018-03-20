845427c58dc70d7380cfa92947ed176d0924a862
Sergi Almar <sergi.almar@gmail.com>
2018-03-20 18:26:38 +0100

    Replace ExpiringSession (deprecated in Spring Session 2) in favor of Session

--- src/main/java/com/sergialmar/wschat/config/WebSocketConfig.java
+++ src/main/java/com/sergialmar/wschat/config/WebSocketConfig.java
@@ -2,14 +2,14 @@ package com.sergialmar.wschat.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.session.ExpiringSession;
+import org.springframework.session.Session;
 import org.springframework.session.web.socket.config.annotation.AbstractSessionWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractSessionWebSocketMessageBrokerConfigurer<ExpiringSession> {
+public class WebSocketConfig extends AbstractSessionWebSocketMessageBrokerConfigurer<Session> {
 	
 	@Override
 	protected void configureStompEndpoints(StompEndpointRegistry registry) {
